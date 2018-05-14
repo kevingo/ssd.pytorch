e9cab9516ac8d3981b52af7bccd6757861691d87
kevingo <kevingo75@gmail.com>
2018-05-14 13:21:34 +0800

    Replace VOC2007/2012 dataset download source

--- data/scripts/VOC2012.sh
+++ data/scripts/VOC2012.sh
@@ -22,7 +22,7 @@ fi
 
 echo "Downloading VOC2012 trainval ..."
 # Download the data.
-curl -LO http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+curl -LO https://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar
 echo "Done downloading."
 
 
